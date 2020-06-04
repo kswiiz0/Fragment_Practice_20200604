d0b5511d7acf43920b8ee559ccb4adc059679641
kswiiz0 <kswiiz0@gmail.com>
2020-06-04 22:14:17 +0900

    뷰페이저 설정을 완료

--- app/src/main/java/com/phis/fragment_practice_20200604/MyViewPagerActivity.kt
+++ app/src/main/java/com/phis/fragment_practice_20200604/MyViewPagerActivity.kt
@@ -2,16 +2,29 @@ package com.phis.fragment_practice_20200604
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentPagerAdapter
+import com.phis.fragment_practice_20200604.adapters.MyPagerAdapter
+import kotlinx.android.synthetic.main.activity_my_view_pager.*
 
-class MyViewPagerActivity : AppCompatActivity() {
+class MyViewPagerActivity : BaseActivity() {
+
+    lateinit var myPagerAdapter: FragmentPagerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_view_pager)
 
-
-
-
+        setValues()
 
     }
+
+    override fun setValues() {
+        myPagerAdapter = MyPagerAdapter(supportFragmentManager)
+        myViewPager.adapter = myPagerAdapter
+    }
+
+    override fun setEvents() {
+
+    }
+
 }
