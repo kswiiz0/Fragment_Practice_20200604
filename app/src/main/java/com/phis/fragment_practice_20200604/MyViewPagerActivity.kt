779342507d9f697929624a2113fab5a75258eeb3
kswiiz0 <kswiiz0@gmail.com>
2020-06-04 22:01:08 +0900

    빈액티비티를 시작점으로 변경 - 뷰페이저 작업의 시작점.

---
+++ app/src/main/java/com/phis/fragment_practice_20200604/MyViewPagerActivity.kt
@@ -0,0 +1,12 @@
+package com.phis.fragment_practice_20200604
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyViewPagerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_view_pager)
+    }
+}
